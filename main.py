a418723eff9c114ead56a4b58e05b623885d69d1
Sebastian Castro <castro_107094@students.pupr.edu>
2021-05-24 14:14:13 -0400

    Added graphics.py to the repository, somehow missed it in the last commit. More functions added for the Vehicle class and more testing done. Objects successfully move from left to right. Will likely have to modify the Projectile class to make objects move the opposite way.

--- main.py
+++ main.py
@@ -35,15 +35,18 @@ class Vehicle:
     
     #Graphical depiction of moving vehicles
     
-    def __init__(self, windowName, angle, velocity, height):
-        self.windowName = ""
-        #self.impact = False
-        self.proj = Projectile(angle, velocity, height)
-        self.icon = Circle(Point(0, height), 0.15)
+    def __init__(self, windowName, height, velocity, start):
+        self.windowName = windowName
+        self.height = height
+        self.velocity = velocity
+        self.start = start
+        
+    def spawn(self, windowName):
+        self.proj = Projectile(0, self.velocity, self.height)
+        self.icon = Circle(Point(self.start , self.height), 0.15)
         self.icon.setFill("red")
         self.icon.setOutline("red")
         self.icon.draw(windowName)
-
 
     def getX(self):
         return self.proj.getX()
@@ -51,39 +54,35 @@ class Vehicle:
     def getY(self):
         return self.proj.getY()
 
-    
-    def updateRight(self, time):
+    def update(self, time):
         self.proj.update(time)
         center = self.icon.getCenter()
         dx = self.proj.getX() - center.getX()
-        self.icon.move(dx, 0)
-
-    def updateLeft(self, time):
-        self.proj.update(time)
-        center = self.icon.getCenter()
-        dx = center.getX() - self.proj.getX()
         self.icon.move(dx, 0)
 
     def moveRight(self):
         center = self.icon.getCenter()
         while (center.getX() < 6):
             update(35)
-            self.updateRight(1/500)
+            self.update(1/500)
             center = self.icon.getCenter()
             print(center.getX())
-        self.undraw()
-        pass
+        self.despawn()
     
-    def moveLeft(self):
-        center = self.icon.getCenter()
-        pass
+##    def moveLeft(self):
+##        center = self.icon.getCenter()
+##        while (center.getX() > 0):
+##            update(35)
+##            self.update(1/500)
+##            center = self.icon.getCenter()
+##            print(center.getX())
+##        self.despawn()
 
     def hasImpacted():
         return self.impact
 
-    def undraw(self):
+    def despawn(self):
         self.icon.undraw()
-        pass
 
 def main():
 
@@ -93,19 +92,24 @@ def main():
 
     drawBackground(win)
 
-    carSim = Vehicle(win, 0, 10, 2)
+    carSim = Vehicle(win, 1, 10, 1)
+    carSim.spawn(win)
     carSim.moveRight()
-
     
-##    for i in range(40):
-##        update(35)
-##        carSim.updateRight(1/500)
+    carSim2 = Vehicle(win, 1.5, -10, 3)
+    carSim2.spawn(win)
 
-        
-    
+    for i in range (400):
+        update(35)
+        carSim2.update(1/500)
 
-    
+
+    alert = Text(Point(3, 3.5), "Click anywhere to exit!")
+    alert.setSize(20)
+    alert.setTextColor("yellow")
+    alert.draw(win)
     win.getMouse()
+    alert.undraw()
     win.close()
 
     pass
